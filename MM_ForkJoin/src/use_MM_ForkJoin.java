9304ec52c180b703c18a4f88039f5c8663fbec81
Vahid Shirvani <vahid_shirvani@hotmail.com>
2014-12-10 11:27:51 +0100

    implemented a forkjoin for summing up values in an array

--- MM_ForkJoin/src/use_MM_ForkJoin.java
+++ MM_ForkJoin/src/use_MM_ForkJoin.java
@@ -1,9 +1,16 @@
+import java.util.concurrent.ForkJoinPool;
+
 
 public class use_MM_ForkJoin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello! World");
+		int size = 100000;
+		int[] array = new int[size];
+		for(int i = 0; i < size; i++)
+			array[i] = (int) (Math.random()*100); 
+		
+		ForkJoinPool fjPool = new ForkJoinPool();
+		int sum = fjPool.invoke(new Sum(array,0,array.length));
+		System.out.println("The sum is " + sum);
 	}
-
 }
