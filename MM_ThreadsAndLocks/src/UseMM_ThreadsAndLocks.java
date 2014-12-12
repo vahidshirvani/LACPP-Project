ec9f200b5959c8d88bc349c6e94e1eafadbc0949
Vahid Shirvani <vahid_shirvani@hotmail.com>
2014-12-12 11:33:05 +0100

    created the base

--- MM_ThreadsAndLocks/src/UseMM_ThreadsAndLocks.java
+++ MM_ThreadsAndLocks/src/UseMM_ThreadsAndLocks.java
@@ -1,8 +1,53 @@
+import java.util.Arrays;
+
 
 public class UseMM_ThreadsAndLocks {
-
+	
+	public static int[][] generateMatrix(int rows, int columns) {
+		int[][] matrix = new int[rows][columns];
+		for(int i = 0; i < rows; i++)
+			for(int j = 0; j < columns; j++)
+				matrix[i][j] = (int) (Math.random() * 10);
+		return matrix;
+	}
+	
+	public static String matrixToString(int[][] matrix) {
+		StringBuilder sb = new StringBuilder();
+		for(int[] row: matrix) {
+			for(int i: row) {
+				sb.append(String.format("%02d", i));
+				sb.append(" ");
+			}
+			sb.append(System.getProperty("line.separator"));
+		}
+		return sb.toString();
+	}
+	
+	public static int[][] SequentialMultiply(int[][] a, int[][] b) {
+		int rowLengthOfA = a[0].length;
+		int columnLengthOfB = b.length;
+		if(rowLengthOfA != columnLengthOfB) {
+			System.out.println("two matrices can not be multiplied");
+			System.exit(1);
+		}
+		
+		int numOfRows = a.length;
+		int numOfColumns = b[0].length;
+		int[][] c = new int[numOfRows][numOfColumns];
+		
+		for(int i = 0; i < numOfRows; i++)
+			for(int j = 0; j <numOfColumns; j++)
+				for(int k = 0; k < rowLengthOfA; k++)
+					c[i][j] = a[i][k] * b[k][j];
+		return c;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		
+//		int[][] a = 
+//		int[][] c = SequentialMultiply(a, b);
+		int[][] a = generateMatrix(5, 5);
+		System.out.println(matrixToString(a));
 	}
 
 }
